d292be7226cb15a724f726b8c117cd1f8b3cc2ec
Zhengmao <malker@qq.com>
2021-02-10 10:13:06 -0800

    switched the axios to node_modules/axios

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Redirect, Route, Switch} from "react-router-dom";
-import axios from 'axios';
+import axios from '../node_modules/axios';
 import Header from "./Header";
 import { Button, Navbar,Nav,Form,FormControl } from '../node_modules/react-bootstrap'
 
