66fffb47bf12bb91e78c7a9b61f36c036d9cef1e
zgstites <zgstites@gmail.com>
2021-02-10 09:19:20 -0800

    Rough draft of OECI login component - should be enough to start on backend, but needs further work (i.e. data validation)

---
+++ src/frontend/src/components/OeciLogin.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+type State = {
+    userId: string;
+    password: string;
+}
+
+class OeciLogin extends React.Component<{}, State> {
+
+    state: State = {
+        userId: "",
+        password: "",
+    };
+
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    };
+
+    componentDidMount(){
+        // document.title is the string in the tab, update it to reflect current component
+        document.title = "OECI Login"
+    };
+
+    onChange = (e: React.FormEvent<HTMLInputElement>): void => {
+        // @ts-ignore // TODO: can't figure out why this.setState({}) makes it unhappy
+        this.setState({ text: e.currentTarget.value });
+        // So, afaik onChange calls handleChange automatically. Some sites recommend this code tidbit be in onChange,
+        // others in handleChange. Not sure why.
+    };
+
+    // gather all the user-entered data and post it for the backend to deal with
+    handleSubmit(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+
+        fetch('/api/form-submit-url', {
+            method: 'POST',
+            body: data,
+        });
+    }
+
+    // HTML(?) for the component
+    render() {
+        return (
+            <form>
+                <label>
+                    User ID:
+                </label>
+                <input
+                    id="userId"
+                    name="oecilogin"
+                    type="text"
+                    value={this.state.userId}
+                    onChange={this.onChange}
+                />
+                <label>
+                    Password:
+                </label>
+                <input
+                    id="password"
+                    name="oecilogin"
+                    type="text"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                />
+                <button> Submit </button>
+            </form>
+        )
+    }
+}
+
+export default OeciLogin;
