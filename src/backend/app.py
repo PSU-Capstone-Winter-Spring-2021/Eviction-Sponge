c6563de323b4e2f41a63f08ddb1c4a410472fef0
Thomas <tnp94@yahoo.com>
2021-02-11 08:51:44 -0800

    Fixed merge conflicts

--- src/backend/app.py
+++ src/backend/app.py
@@ -8,24 +8,8 @@ FRONTEND_BUILD_DIR = path.abspath(path.join(path.dirname(__file__), "..", "front
 def create_app():
     app = Flask(__name__, static_folder=FRONTEND_BUILD_DIR)
 
-<<<<<<< HEAD
-    if __name__ == '__main__':
-      app.run(host = 'localhost', port = '5000')
-      
-    # Main page
-    @app.route('/')
-    def index():
-      return 'Homepage'
-
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-=======
     hello.register(app)
     static.register(app)
->>>>>>> backendstart
 
     return app
 
