4ed20fd66b22b5b500fbe3472ebce0d25a948d1a
Thomas <tnp94@yahoo.com>
2021-02-15 22:11:53 -0800

    Added functionality to receive the user input from the OeciLogin component

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,5 +1,5 @@
 from os import path
-from flask import Flask
+from flask import Flask, request
 
 from endpoints import hello, static
 
@@ -8,8 +8,14 @@ FRONTEND_BUILD_DIR = path.abspath(path.join(path.dirname(__file__), "..", "front
 def create_app():
     app = Flask(__name__, static_folder=FRONTEND_BUILD_DIR)
 
+    @app.route('/api/form-submit-url', methods=['POST'])
+    def handleLogin():
+        print("login attempted with user", request.form['oecilogin'], "and password", request.form['oecipassword'])
+        return "You attempted to login"
+
     hello.register(app)
     static.register(app)
+
 
     return app
 
