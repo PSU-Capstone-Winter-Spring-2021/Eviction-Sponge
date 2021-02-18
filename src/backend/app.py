7fb458a296142a0047830f2999ab209eaff2d067
Thomas <tnp94@yahoo.com>
2021-02-17 19:23:54 -0800

    Added build directory and removed redundant route

--- src/backend/app.py
+++ src/backend/app.py
@@ -3,29 +3,30 @@ from flask import Flask, request, make_response
 
 from endpoints import hello, static, oeci_login
 
-FRONTEND_BUILD_DIR = path.abspath(path.join(path.dirname(__file__), "..", "frontend"))
+FRONTEND_BUILD_DIR = path.abspath(path.join(path.dirname(__file__), "..", "frontend", "build"))
 
 def create_app():
     app = Flask(__name__, static_folder=FRONTEND_BUILD_DIR)
 
-    @app.route('/api/form-submit-url', methods=['POST'])
-    def handleLogin():
-        print("login attempted with user", request.form['oecilogin'], "and password", request.form['oecipassword'])
-        response = make_response()
-        response.set_cookie(
-            "oeci_token",
-            #secure=os.getenv("TIER") == "production",
-            httponly=False,
-            samesite="strict",
-            #value=encrypted_credentials,
-            value="Test Cookie. <TODO> Set encrypted credentials here"
-        )
-        return response, 201
+    # @app.route('/api/form-submit-url', methods=['POST'])
+    # def handleLogin():
+    #     print("login attempted with user", request.form['oecilogin'], "and password", request.form['oecipassword'])
+    #     response = make_response()
+    #     response.set_cookie(
+    #         "oeci_token",
+    #         #secure=os.getenv("TIER") == "production",
+    #         #httponly=False,
+    #         #samesite="strict",
+    #         #value=encrypted_credentials,
+    #         value="Test Cookie. <TODO> Set encrypted credentials here"
+    #     )
+    #     response.set_cookie("test", "success")
+    #     return response, 201
+
 
     hello.register(app)
     static.register(app)
     oeci_login.register(app)
-
 
     return app
 
