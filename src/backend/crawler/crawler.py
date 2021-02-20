adeb197cee5673718708110ef9033d0d4e5c243e
Caillie Juergens <caillie.juergens@gmail.com>
2021-02-20 01:09:18 -0800

    attempt_login function working and reports a success or failure to login with credentials to the OECI website

--- src/backend/crawler/crawler.py
+++ src/backend/crawler/crawler.py
@@ -6,21 +6,26 @@ import requests
 # attempts to login to the OECI website
 
 class Crawler:
+    def __init__(self):
+        pass
+
     @staticmethod
-    def attempt_login():
+    def attempt_login(username, password):
         url = 'https://publicaccess.courts.oregon.gov/PublicAccessLogin/login.aspx'
-        payload = {'username': 'QQLMUL01', 'password': 'QQLMUL01'}
-        # , 'ValidateUser': '1',
-        # 'dbKeyAuth': 'JusticePA', 'SignOn': 'Sign+On'}
+        payload = {'UserName': username, 'Password': password , 'ValidateUser': '1',
+         'dbKeyAuth': 'JusticePA', 'SignOn': 'Sign+On'}
         r = requests.post(url, payload)
         content = r.text
-        print content
+        if "Case Records" in content:
+            # success
+            return 0
+        else:
+            # failure
+            return 1
 
 # for testing only
 def main():
-    Crawler.attempt_login()
-    #"Request Rejected"
-
+    Crawler.attempt_login('QQLMUL01', 'QQLMUL01')
 
 if __name__ == "__main__":
     main()
