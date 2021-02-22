6f40a3fe35516b4b95fc20025b8b6af816a7c5e4
Zhengmao <malker@qq.com>
2021-02-22 00:21:19 -0800

    save the navbar  to file compoments navbar.jsx

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,7 +3,8 @@ import { Redirect, Route, Switch} from "react-router-dom";
 import axios from '../node_modules/axios';
 //import Header from "./Header";
 import OECILogin from "./components/OeciLogin";
-import { Button, Navbar,Nav,Form,FormControl } from '../node_modules/react-bootstrap'
+import navbar from "./components/navbar";
+// import { Button, Navbar,Nav,Form,FormControl } from '../node_modules/react-bootstrap'
 
 function App() {
   return (
@@ -11,48 +12,10 @@ function App() {
   <OECILogin />
     <div className="App">
       <header className="App-header">
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
-    </Form>
-  </Navbar>
-  <br />
-  <Navbar bg="primary" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-light">Search</Button>
-    </Form>
-  </Navbar>
-
-  <br />
-  <Navbar bg="light" variant="light">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-primary">Search</Button>
-    </Form>
-  </Navbar>
       </header>
     </div>
   </>
+
   );
 }
 
