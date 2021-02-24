43b85c930661dc4064fe9b682e67a9203f0f75e3
Caillie Juergens <caillie.juergens@gmail.com>
2021-02-23 21:25:07 -0800

    Beginnings of form_filling.py added for filling PDF's. Other variables may be added later

---
+++ src/backend/form_filling.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class FormData:
+    county: str
+    case_number: str
+
+    plaintiff_line1: str
+    plaintiff_line2: str
+
+    defendant_line1: str
+    defendant_line2: str
+    defendant_line3: str
+    defendant_line4: str
