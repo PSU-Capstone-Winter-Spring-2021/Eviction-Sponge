2daff4764badd628f80bd532a5d935f36bb0eb25
Danford <danford.compton@gmail.com>
2021-03-01 16:33:31 -0800

    Checkbox works, it just doesn't show up when clicked for some reason.

--- src/backend/form_filling.py
+++ src/backend/form_filling.py
@@ -19,6 +19,47 @@ class FormData:
     city_state_zip: str
     phone_number: str
 
-
-
-
+# Everything that says none is a checkbox, and I have not figured out how to make those work yet
+@dataclass 
+class PDF_data_template:
+    COUNTY_1: str
+    COUNTY_2: str
+    PLAINTIFF_1_1: str
+    PLAINTIFF_1_2: str
+    DEFENDANT_1_1: str
+    DEFENDANT_1_2: str
+    DEFENDANT_1_3: str
+    DEFENDANT_1_4: str
+    PLAINTIFF_2_1: str
+    PLAINTIFF_2_2: str
+    DEFENDANT_2_1: str
+    DEFENDANT_2_2: str
+    DEFENDANT_2_3: str
+    DEFENDANT_2_4: str
+    MOTION: str
+    CASE: str
+    CASE_NO_1: str
+    CASE_NO_2: str
+    DISMISSAL: None
+    RESTITUTION: None
+    MONEY: None
+    JUDGEMENT: None
+    STIPULATION: None
+    TERMS: None
+    DATE_OF_JUDGEMENT: str
+    DATE: str
+    NAME_PRINTED: str
+    ADDRESS: str
+    CITY_STATE_ZIP: str
+    PHONE: str
+    DATE_2: str
+    PLAINTIFF_ADDRESS_1: str
+    PLAINTIFF_ADDRESS_2: str
+    PLAINTIFF_ADDRESS_3: str
+    DATE_3: str
+    DEFENDANT_NAME_1: str
+    DEFENDANT_NAME_2: str
+    DEFENDANT_ADDRESS: str
+    PLAINTIFF_NAME: str
+    DO_NOT_FILL: str
+    DO_NOT_CLICK: None
