bd960079c26533cc8acd09fcfceabc7624f0a68f
Zayne <zgstites@gmail.com>
2021-03-08 09:15:32 -0800

    Merged the two crawler.py's and updated other files to account for new path

--- src/backend/crawler/crawler.py
+++ src/backend/crawler/crawler.py
@@ -1,8 +1,16 @@
 import requests
 
 
+class UnableToReachOECI(Exception):
+    pass
+
+
+class InvalidLoginCreds(Exception):
+    pass
+
+
 # attempt login function that takes in a username and password and
-# returns a success (0) or failure (1)
+# returns a success (0) or throws an exception detailing why failure occured
 # attempts to login to the OECI website
 
 class Crawler:
@@ -12,20 +20,18 @@ class Crawler:
     @staticmethod
     def attempt_login(username, password):
         url = 'https://publicaccess.courts.oregon.gov/PublicAccessLogin/login.aspx'
-        payload = {'UserName': username, 'Password': password , 'ValidateUser': '1',
-         'dbKeyAuth': 'JusticePA', 'SignOn': 'Sign+On'}
+        payload = {'UserName': username, 'Password': password, 'ValidateUser': '1',
+                   'dbKeyAuth': 'JusticePA', 'SignOn': 'Sign+On'}
         r = requests.post(url, payload)
         content = r.text
         if "Case Records" in content:
             # success
-            return 0
+            return content
+        elif "Oregon eCourt is temporarily unavailable due to maintenance" in content:
+            raise UnableToReachOECI
         else:
-            # failure
-            return 1
+            raise InvalidLoginCreds
 
-# for testing only
-def main():
-    pass
 
 if __name__ == "__main__":
     main()
