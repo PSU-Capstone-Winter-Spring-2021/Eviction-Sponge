9d99ac4c9516b931dad86fdeedc08cb40de6fe3f
Danford <danford.compton@gmail.com>
2021-03-08 18:32:38 -0800

    still has pdfrw error

--- src/backend/pdf_creator.py
+++ src/backend/pdf_creator.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 import pdfrw
 import datetime
 from form_filling import FormData, PDF_form_template
+from src.backend.tests import pdf_test_dicts
+
 
 
 # Data dict names:
@@ -29,57 +31,44 @@ from form_filling import FormData, PDF_form_template
 # PLAINTIFF_ADDRESS_CITYSTATEZIP_PHONE -single line [Address    CityStateZip     PHONE]
 # OBJECTION_DATE - don't fill
 
+TEMPLATE_PATH = 'template.pdf'
+OUTPUT_PATH = 'output.pdf'
+PDF_FORM_LOCATION = 'src/backend/files/EvictionPDF.pdf'
 
 @dataclass
 class CreatePDF:
-    TEMPLATE_PATH = 'template.pdf'
-    OUTPUT_PATH = 'output.pdf'
 
-    test_dict = {
-        'COUNTY_1': 'Multnomah',
-        'COUNTY_2': 'Multnomah',
-        'PLAINTIFF_1_1': "The Burger King",
-        'PLAINTIFF_1_2': "",
-        'DEFENDANT_1_1': 'Hamburgler',
-        'DEFENDANT_1_2': "Grimace",
-        'DEFENDANT_1_3': "",
-        'DEFENDANT_1_4': "",
-        'PLAINTIFF_2_1': "The Burger King",
-        'PLAINTIFF_2_2': "",
-        'DEFENDANT_2_1': 'Hamburgler',
-        'DEFENDANT_2_2': "Grimace",
-        'DEFENDANT_2_3': "",
-        'DEFENDANT_2_4': "",
-        'MOTION': 'Hamburgler',
-        'CASE': 'Burger King v Hamburgler',
-        'CASE_NO_1': '8675309',
-        'CASE_NO_2': '8675309',
-        'DISMISSAL': False,
-        'RESTITUTION': True,
-        'MONEY': True,
-        'JUDGEMENT': True,
-        'STIPULATION': False,
-        'TERMS': False,
-        'DATE_OF_JUDGEMENT': '5/29/2015',
-        'DATE': datetime.date.today(),
-        'NAME_PRINTED': 'Hamburgler',
-        'ADDRESS': '10050 SW Barbur Blvd',
-        'CITY_STATE_ZIP': 'Portland OR, 97219',
-        'PHONE': '(503) 246-6711',
-        'DATE_2': datetime.date.today(),
-        'PLAINTIFF_ADDRESS_1': '11539 SW',
-        'PLAINTIFF_ADDRESS_2': 'Pacific Hwy',
-        'PLAINTIFF_ADDRESS_3': 'Tigard, OR 97223',
-        'DATE_3': datetime.date.today(),
-        'DEFENDANT_NAME_1': "Hamburgler",
-        'DEFENDANT_NAME_2': "Hamburgler",
-        'DEFENDANT_ADDRESS': '10050 SW Barbur Blvd',
-        'PLAINTIFF_NAME': "The Burger King",
-        'DO_NOT_FILL': "",
-        'DO_NOT_CLICK': False
-    }
+    def fit_address(self, address):
+        first_line = 22
+        second_line = 72
+        third_line = 65
+        first_line_string = ''
+        second_line_string = ''
+        third_line_string = ''
+        address_list = address.split()
+        for stuff in address_list:
+            if len(stuff) > first_line:
+                if len(stuff) > second_line:
+                    if len(stuff) > third_line:
+                        raise Exception("Plainiff address is too long")
+                    else:
+                        third_line_string += stuff + " "
+                        third_line = third_line - len(stuff) - 1
+                        continue
+                else:
+                    second_line_string += stuff + " "
+                    second_line = second_line - len(stuff) - 1
+                    continue
+            else:
+                first_line_string += stuff + " "
+                first_line = first_line - len(stuff) - 1
+                continue
+
+        address_output = [first_line_string, second_line_string, third_line_string]
+        return address_output
 
     def data_dict_to_pdf_dict(self, input_dict):
+        address_list = self.fit_address(input_dict['plaintiff_address'])
         output = PDF_form_template(
             DEFENDANT_ADDRESS=input_dict['def_mailing_address'],
             CITY_STATE_ZIP=input_dict['city_state_zip'],
@@ -97,9 +86,9 @@ class CreatePDF:
             DEFENDANT_2_2=input_dict['defendant_line2'],
             DEFENDANT_2_3=input_dict['defendant_line3'],
             DEFENDANT_2_4=input_dict['defendant_line4'],
-            DATE=datetime.date.today(),
-            DATE_2=datetime.date.today(),
-            DATE_3=datetime.date.today(),
+            DATE=str(datetime.date.today()),
+            DATE_2=str(datetime.date.today()),
+            DATE_3=str(datetime.date.today()),
             CASE=input_dict['case_name'],
             CASE_NO_1=input_dict['case_number'],
             CASE_NO_2=input_dict['case_number'],
@@ -108,10 +97,20 @@ class CreatePDF:
             DEFENDANT_NAME_3=input_dict['defendant_line1'],
             DATE_OF_JUDGEMENT=input_dict['date_of_judgement'],
             PHONE=input_dict['phone_number'],
-            PLAINTIFF_NAME=input_dict['plaintiff_line1']
+            PLAINTIFF_NAME=input_dict['plaintiff_line1'],
+            DISMISSAL=input_dict['dismissal'],
+            RESTITUTION=input_dict['restitution'],
+            MONEY=input_dict['money'],
+            JUDGEMENT=input_dict['judgement'],
+            STIPULATION=input_dict['stipulation'],
+            TERMS=input_dict['terms'],
+            DO_NOT_FILL="",
+            DO_NOT_CLICK=False,
+            PLAINTIFF_ADDRESS_3=address_list[0],
+            PLAINTIFF_ADDRESS_2=address_list[1],
+            PLAINTIFF_ADDRESS_1=address_list[2]
         )
-        # sort out plaintiff address here @ input['mailing_address']
-        # DISMISSAL, RESTITUTION, MONEY, JUDGEMENT, STIPULATION, TERMS all bool
+        return output
 
     def fill_pdf(self, input_pdf_path, output_pdf_path, data_dict):
         ANNOT_KEY = '/Annots'
@@ -141,5 +140,12 @@ class CreatePDF:
 
         pdfrw.PdfWriter().write(output_pdf_path, template_pdf)
 
-    fill_pdf("src/backend/files/Fillable_FED-Motion-SetAside-2020-01-01.pdf", "src/backend/files/test_result.pdf",
-             test_dict)
+    def PDF_filler(self, input_dict):
+        output = CreatePDF.data_dict_to_pdf_dict(self, input_dict)
+        CreatePDF.fill_pdf(self, PDF_FORM_LOCATION,
+                           "src/backend/files/"+input_dict['plaintiff_line1']+".pdf",
+                           output)
+
+
+taco = CreatePDF()
+CreatePDF.PDF_filler(taco, pdf_test_dicts.test_dict_2)
