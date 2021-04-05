dda415bd6e596ad2f97ebfd9a7d58f263a3cba7d
Caillie Juergens <caillie.juergens@gmail.com>
2021-04-05 14:17:37 -0700

    Navigation to sections of text of manual added to the top of the frontend Manual component. Tested - appears to be working correctly

--- src/frontend/src/components/Manual.jsx
+++ src/frontend/src/components/Manual.jsx
@@ -13,16 +13,76 @@ class Manual extends React.Component {
         document.title = "Manual"
     };
 
-    // HTML(?) for the component
+    // HTML for the component
     render() {
         return (
-            <textarea>
-                Step 1: Hit Search, if it's been implemented
-                Step 2: Login
-                Step 3: Search yourself
-                Step 4: ???
-                Step 5: Profit!
-            </textarea>
+        <>
+            <nav>
+            <ul className="list">
+              <li>
+                <a href="#introduction">
+                  Introduction
+                </a>
+              </li>
+              <li>
+                <a href="#information">
+                  General Information
+                </a>
+              </li>
+              <li>
+                <a href="#overview">
+                  Use EvictionSponge
+                </a>
+              </li>
+              <li>
+                <a href="#assumptions">
+                  Assumptions
+                </a>
+              </li>
+              <li>
+                <a href="#search">
+                  Search
+                </a>
+              </li>
+              <li>
+                <a href="#file">
+                  File for Eviction Expungement
+                </a>
+              </li>
+            </ul>
+            </nav>
+            <a name="introduction">
+                <p>
+                    Introduction
+                </p>
+            </a>
+            <a name="information">
+                <p>
+                    General Information
+                </p>
+            </a>
+            <a name="overview">
+                <p>
+                    Use EvictionSponge
+                </p>
+            </a>
+            <a name="assumptions">
+                <p>
+                    Assumptions
+                </p>
+            </a>
+            <a name="search">
+                <p>
+                    Search
+                </p>
+            </a>
+            <a name="file">
+                <p>
+                    File for Eviction Expungement
+                </p>
+            </a>
+        </>
+
         )
     }
 }
