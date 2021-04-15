f11445380d690e75a22ebce9c9bbbb01254c0218
Danford <danford.compton@gmail.com>
2021-04-14 19:51:36 -0700

    changed login to redirect to search, return 302

--- src/backend/endpoints/oeci_login.py
+++ src/backend/endpoints/oeci_login.py
@@ -1,5 +1,5 @@
 from flask.views import MethodView
-from flask import request, make_response, current_app, abort, jsonify
+from flask import request, make_response, current_app, abort, jsonify, redirect
 
 from crawler.crawler import Crawler, UnableToReachOECI, InvalidLoginCreds
 
@@ -46,7 +46,7 @@ class OeciLogin(MethodView):
         except InvalidLoginCreds:
             error(401, "Invalid login credentials")
 
-        return response, 201
+        return redirect('/search', code=302)
 
 
 def register(app):
