f968faa0450f7751f2475c76feed9108250d215b
Caillie Juergens <caillie.juergens@gmail.com>
2021-05-01 00:17:40 -0700

    Email sends but password would need to be hard coded.

--- src/backend/endpoints/partnerspage.py
+++ src/backend/endpoints/partnerspage.py
@@ -1,6 +1,8 @@
 from flask.views import MethodView
 from flask import request, make_response, current_app, abort, jsonify, json
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 def error(code, message):
     current_app.logger.error("code %i %s" % (code, message), stack_info=True)
@@ -13,47 +15,34 @@ class PartnersPage(MethodView):
         if data is None:
             error(400, "Missing one or more required fields")
 
-        sender = 'from@fromdomain.com'
-        receivers = ['to@domain.com']
-
-        message = """From: From Person <from@fromdomain.com>
-        To: To Person <to@todomain.com>
-        Subject: SMTP e-mail test
-
-        This is a test e-mail message.
-        """
-
-        try:
-            smtpObj = smtplib.SMTP('localhost')
-            smtpObj.sendmail(sender, receivers, message)
-            print
-            "Successfully sent email"
-        except smtplib.SMTPException:
-            print
-            "Error: unable to send email"
+def register(app):
+    app.add_url_rule("/partners?", view_func=PartnersPage.as_view("partners?"))
 
 def main():
-    sender = 'from@fromdomain.com'
-    receivers = ['to@domain.com']
+    #This works to send an email but requires the password to be hard coded..
 
-    message = """From: From Person <from@fromdomain.com>
-            To: To Person <to@todomain.com>
-            Subject: SMTP e-mail test
-
-            This is a test e-mail message.
-            """
-
-    try:
-        smtpObj = smtplib.SMTP('localhost')
-        smtpObj.sendmail(sender, receivers, message)
-        print
-        "Successfully sent email"
-    except smtplib.SMTPException:
-        print
-        "Error: unable to send email"
+    mail_content = '''Hello,
+    This is a simple mail. There is only text, no attachments are there The mail is sent using Python SMTP library.
+    Thank You'''
+    #The mail addresses and password
+    sender_address = 'sender@gmail.com'
+    sender_pass = 'password'
+    receiver_address = 'receiver@gmail.com'
+    #Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
+    #The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(sender_address, sender_pass) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('Mail Sent')
 
 if __name__ == "__main__":
     main()
-
-def register(app):
-    app.add_url_rule("/partners?", view_func=PartnersPage.as_view("partners?"))
