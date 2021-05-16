567f0785f2078ae3c4f683240d198a033e34e9b7
Zayne <zgstites@gmail.com>
2021-05-15 21:28:52 -0700

    Started testing for demo_search

--- tests/unitTests/endpointTests/demo_search_test.py
+++ tests/unitTests/endpointTests/demo_search_test.py
@@ -1,12 +1,14 @@
 import csv
 import os
 import sys
+from datetime import datetime
+from unittest import mock
+from flask import json
 
 import pytest
-import builtins
-
 
 # Need this little tidbit for pytest to work
+from src.backend import eligibility_eval
 from src.backend.endpoints.search import Search
 
 myPath = os.path.dirname(os.path.abspath(__file__))
@@ -23,7 +25,7 @@ def client():
         yield client
 
 
-def search(client, first_name, last_name, middle_name):
+def demo_search(client, first_name, last_name, middle_name):
     data = {
         'first_name': first_name,
         'last_name': last_name,
@@ -32,16 +34,29 @@ def search(client, first_name, last_name, middle_name):
     return client.post('/demo', json=data, follow_redirects=True)
 
 
-def mock_open(path, newline):
-    return None
-
-
 def mock_csv_reader(file, delimiter):
-    return ['case num', 'style', 'location', 'type', 'status', 'closed_date', 'Judgement', 'True', 'Eligible']
+    return ['case num', 'style', 'location', 'type', 'status', 'complaint date',
+            '10/10/2010', 'judgement']
+
+
+def mock_is_eligible(current_status, closed_date, judgements):
+    return True, "Eligible"
+
+
+def mock_datetime_strptime(date_string, format):
+    return date_string
+
+
+def mock_json_dump(data):
+    return data
 
 
 def test_successful_demo(client, monkeypatch):
-    monkeypatch.setitem(__builtins__, 'open', mock_open)
-    monkeypatch.setitem(csv, "reader", mock_csv_reader)
-    #cases = list((search(client, "first_name", "last_name", "middle_name")).json)
-    #assert cases == 5
+    monkeypatch.setattr(csv, 'reader', mock_csv_reader)
+    monkeypatch.setattr(json, 'dumps', mock_json_dump)
+    monkeypatch.setattr(eligibility_eval, 'is_eligible', mock_is_eligible)
+    # datetime.strptime('10/10/2010', '%m/%d/%Y').date() works fine here...
+
+    with mock.patch('builtins.open', mock.mock_open(read_data=None)):
+        cases = (demo_search(client, 'first_name', 'last_name', 'middle_name'))
+        assert cases == 5
