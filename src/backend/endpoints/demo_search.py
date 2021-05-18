5e41018cfe47fab19ed23caa867ef1263b06c87e
Zayne <zgstites@gmail.com>
2021-05-18 13:39:07 -0700

    Demo search test now working

--- src/backend/endpoints/demo_search.py
+++ src/backend/endpoints/demo_search.py
@@ -16,18 +16,22 @@ def split_judgements_string(judgements):
     return judgements.split(', ')
 
 
+#  Made this a function for ease of testing, since patching strptime is a huge hassle for some reason
+def string_to_date(string, format):
+    return datetime.strptime(string, format).date()
+
+
 # This class serves to demonstrate the website without concern about displaying real eviction cases
 # From an outside perspective, it functions the same as the search endpoint, but the data is made up
 class DemoSearch(MethodView):
     def post(self):
         search_results = {}
         path = os.path.relpath('backend\\data\\demo_search_data.csv', os.path.dirname(__file__))
-        #with open(path, newline='\n') as demoFile:
-        with open(path) as demoFile:
+        with open(path, newline='\n') as demoFile:
             demoData = csv.reader(demoFile, delimiter=';')
             for fakeCase in demoData:
                 eligibility = eligibility_eval.is_eligible(fakeCase[4],
-                                                           datetime.strptime(fakeCase[6], '%m/%d/%Y').date(),
+                                                           string_to_date(fakeCase[6], '%m/%d/%Y'),
                                                            split_judgements_string(fakeCase[7]))
                 key = fakeCase[0]
                 value = {'style': fakeCase[1],
--- tests/unitTests/endpointTests/demo_search_test.py
+++ tests/unitTests/endpointTests/demo_search_test.py
@@ -1,15 +1,13 @@
 import csv
 import os
 import sys
-from datetime import datetime
 from unittest import mock
 from flask import json
-
+from src.backend import eligibility_eval
+from src.backend.endpoints import demo_search
 import pytest
 
 # Need this little tidbit for pytest to work
-from src.backend import eligibility_eval
-from src.backend.endpoints.search import Search
 
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
@@ -25,7 +23,7 @@ def client():
         yield client
 
 
-def demo_search(client, first_name, last_name, middle_name):
+def search(client, first_name, last_name, middle_name):
     data = {
         'first_name': first_name,
         'last_name': last_name,
@@ -35,8 +33,8 @@ def demo_search(client, first_name, last_name, middle_name):
 
 
 def mock_csv_reader(file, delimiter):
-    return ['case num', 'style', 'location', 'type', 'status', 'complaint date',
-            '10/10/2010', 'judgement']
+    return [['case-num', 'style', 'location', 'type', 'status', 'complaint-date',
+             '10/10/2010', 'judgement']]
 
 
 def mock_is_eligible(current_status, closed_date, judgements):
@@ -47,16 +45,21 @@ def mock_datetime_strptime(date_string, format):
     return date_string
 
 
-def mock_json_dump(data):
-    return data
-
-
 def test_successful_demo(client, monkeypatch):
+    monkeypatch.setattr(demo_search, "string_to_date", mock_datetime_strptime)
     monkeypatch.setattr(csv, 'reader', mock_csv_reader)
-    monkeypatch.setattr(json, 'dumps', mock_json_dump)
     monkeypatch.setattr(eligibility_eval, 'is_eligible', mock_is_eligible)
-    # datetime.strptime('10/10/2010', '%m/%d/%Y').date() works fine here...
 
     with mock.patch('builtins.open', mock.mock_open(read_data=None)):
-        cases = (demo_search(client, 'first_name', 'last_name', 'middle_name'))
-        assert cases == 5
+        response = (search(client, 'first_name', 'last_name', 'middle_name'))
+        assert response.status_code == 200
+        #  json.loads takes a json string dictionary and converts it to a python dictionary, undoing the json.dumps()
+        contents = json.loads(response.data.decode("utf-8"))
+        assert contents == {'case-num': {'closed_date': '10/10/2010',
+                                         'complaint_date': 'complaint-date',
+                                         'eligibility': [True, 'Eligible'],
+                                         'judgements': ['judgement'],
+                                         'location': 'location',
+                                         'status': 'status',
+                                         'style': 'style',
+                                         'violation_type': 'type'}}
