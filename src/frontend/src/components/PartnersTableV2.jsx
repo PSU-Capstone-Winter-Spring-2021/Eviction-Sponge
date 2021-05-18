29f9f23fce2495a9b950f51eb685b13fb4c37713
lvoruz <souldestruction45@gmail.com>
2021-05-17 18:55:24 -0700

    cleaning up

--- src/frontend/src/components/PartnersTableV2.jsx
+++ src/frontend/src/components/PartnersTableV2.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 import partnerData from "../data/partnerData.json";
