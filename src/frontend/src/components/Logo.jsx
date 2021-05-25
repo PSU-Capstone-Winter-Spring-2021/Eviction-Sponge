f4972c1e6e7a408869e2a217bb94825682716cb3
Ping-Chun Chung <bingo61016@gmail.com>
2021-05-24 19:24:37 -0700

    Fixed img width attribute of the logo

--- src/frontend/src/components/Logo.jsx
+++ src/frontend/src/components/Logo.jsx
@@ -4,7 +4,7 @@ import logoImage from "../logo.png";
 console.log(logoImage);
 
 function Logo(){
-    return <img src={logoImage} alt="Logo" width="100" height="100" />;
+    return <img src={logoImage} alt="Logo" width="300" height="100" />;
 }
 
 export default Logo;
