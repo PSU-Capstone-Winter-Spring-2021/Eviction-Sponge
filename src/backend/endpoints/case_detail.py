7e1a0d538dda6ea51af5b664976441460fbe387f
Ping-Chun Chung <bingo61016@gmail.com>
2021-05-26 21:03:26 -0700

    Fixed concatenate int and string without type conversion

--- src/backend/endpoints/case_detail.py
+++ src/backend/endpoints/case_detail.py
@@ -7,17 +7,17 @@ from crawler.crawler import Crawler
 class CaseDetail(MethodView):
     def get(self, case_id):
         # We already have the page cached in Crawler, so just grab the html from there
-        url = 'https://publicaccess.courts.oregon.gov/PublicAccessLogin/CaseDetail.aspx?CaseID=' + str(case_id)
+        url = f"https://publicaccess.courts.oregon.gov/PublicAccessLogin/CaseDetail.aspx?CaseID={case_id}"
         page_html = Crawler.fetch_case_detail_link(url)
 
         if not page_html:
-            return "Unable to retrieve case detail page for case with ID " + case_id + "."
+            return f"Unable to retrieve case detail page for case with ID {case_id}."
 
         soup = BeautifulSoup(page_html.text, "html.parser")
 
         # Strip all the links
-        for link in soup.findAll('a'):
-            link['href'] = "#"
+        for link in soup.findAll("a"):
+            link["href"] = "#"
 
         return soup.prettify()
 
