2ea66e29dc021f60b8f2dde99d1ba2d72795f59f
Thomas <tnp94@yahoo.com>
2021-05-26 20:07:57 -0700

    Fixed missing imports

--- src/frontend/src/components/DemoPage.js
+++ src/frontend/src/components/DemoPage.js
@@ -1,4 +1,7 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Search from './Search';
+import Container from '@material-ui/core/Container';
 
 class DemoPage extends React.Component {
     componentDidMount(){
