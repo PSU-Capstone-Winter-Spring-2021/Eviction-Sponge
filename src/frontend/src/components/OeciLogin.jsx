d3d6c8d007d5587c834171d3ee9d7c51bae6443c
lvoruz <souldestruction45@gmail.com>
2021-05-31 14:49:57 -0700

    redesigned login page and added information on eCourt's maintenance schedule

--- src/frontend/src/components/OeciLogin.jsx
+++ src/frontend/src/components/OeciLogin.jsx
@@ -45,34 +45,71 @@ class OeciLogin extends React.Component {
     // HTML(?) for the component
     render() {
         return (
-            <main className="oeci-login-container p-3">
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    User ID:
-                </label>
-                <input
-                    id="userId"
-                    name="oecilogin"
-                    type="text"
-                    placeholder="User ID"
-                    value={this.state.userId}
-                    onChange={this.onChange}
-                />
-                <label>
-                    Password:
-                </label>
-                <input
-                    id="password"
-                    name="oecipassword"
-                    type="password"
-                    placeholder = "Password"
-                    value={this.state.password}
-                    onChange={this.onChange}
-                />
-                <input type="submit" value="Log In" />
+            <main className="row">
+            <form className="col-md-6 bg-darker-green min-vh-100" onSubmit={this.handleSubmit}>
+                <div className="d-flex flex-row text-color-white mt-5 mb-3">
+                    <h4 className="col">OECI Login</h4>
+                </div>
+                <div className="d-flex flex-row justify-content-center text-color-white mb-5">
+                    <p className="col" style={{maxWidth:"500px"}}>Login to Oeci to search and analyse eviction records for expungement</p>
+                </div>
+                <div className="d-flex flex-row justify-content-center mt-5 mb-4 text-left">
+                    <label className="col-lg-3 text-color-white font-weight-bold">
+                        User ID:
+                    </label>
+                    <input
+                        className="col-lg-3"
+                        id="userId"
+                        name="oecilogin"
+                        type="text"
+                        placeholder="User ID"
+                        value={this.state.userId}
+                        onChange={this.onChange}
+                    />
+                </div>
+                <div className="d-flex flex-row justify-content-center mb-4 text-left">
+                    <label className="col-lg-3 text-color-white font-weight-bold">
+                        Password:
+                    </label>
+                    <input
+                        className="col-lg-3"
+                        id="password"
+                        name="oecipassword"
+                        type="password"
+                        placeholder = "Password"
+                        value={this.state.password}
+                        onChange={this.onChange}
+                    />
+                </div>
+                <div className = "d-flex flex-row justify-content-center mb-5">
+                    <div className="col-lg-3"></div>
+                    <input className="col-lg-3 btn btn-success" type="submit" value="Log In" />
+                </div>
+                <div className = "d-flex flex-row justify-content-center text-color-white">
+                    <p className="col"  style={{maxWidth:"500px"}}>
+                        The eCourt site is offline during the 4th weekend of each month between
+                        6 PM PST on Friday until noon on Sunday. During this time, record search
+                        will not function.
+                    </p>
+                </div>
             </form>
-            <div>
-                <h1>hello</h1>
+            <div className="col-md-6 bg-light min-vh-100 align-self-start">
+                <div className="d-flex flex-row justify-content-center mt-4">
+                    <h1 className="col">New Here?</h1>
+                </div>
+                <div className="d-flex flex-row justify-content-center mt-4">
+                    <p className="col" style={{maxWidth:"500px"}}>
+                        EvictionSponge requires an OECI account to function.
+                    </p>
+                </div>
+                <div className="d-flex flex-row justify-content-center mt-4">
+                    <p className="col" style={{maxWidth:"500px"}}>
+                        If you don't have one already, you can <br/>
+                        <a href="https://www.courts.oregon.gov/services/online/Pages/ojcin-signup.aspx">
+                            purchase a subscription here.
+                        </a>
+                    </p>
+                </div>
             </div>
             </main>
         )
