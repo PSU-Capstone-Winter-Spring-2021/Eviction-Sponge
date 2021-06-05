0d586946e287dac27096dac6db00b6034fbf794d
Thomas <tnp94@yahoo.com>
2021-06-04 23:48:54 -0700

    Restore double backslashes to pre-fix attempt

--- src/backend/endpoints/demo_search.py
+++ src/backend/endpoints/demo_search.py
@@ -27,7 +27,7 @@ def string_to_date(string, format):
 class DemoSearch(MethodView):
     def post(self):
         search_results = []
-        path = os.path.relpath('backend\data\demo_search_data.csv', os.path.dirname(__file__))
+        path = os.path.relpath('backend\\data\\demo_search_data.csv', os.path.dirname(__file__))
         with open(path, newline='\n') as demoFile:
             demoData = csv.reader(demoFile, delimiter=';')
             for fakeCase in demoData:
