2a87f361a9e1c465d8d6d470b6b13694827ee14b
sam youngs <youngs3@pdx.edu>
2021-06-06 15:40:27 -0700

    adding image and about text

--- src/frontend/src/components/About.jsx
+++ src/frontend/src/components/About.jsx
@@ -63,10 +63,10 @@ class About extends React.Component {
                           </div>
                           <div class="col-sm-6">
                             <div className="col-md-12 well">
-                            <p><img src="https://www.kindpng.com/picc/m/144-1447559_profile-icon-missing-profile-picture-icon-hd-png.png"
+                            <p><img src="https://media-exp1.licdn.com/dms/image/C5603AQGERE1qc0M9wA/profile-displayphoto-shrink_800_800/0/1619299758622?e=1628726400&v=beta&t=Al9PvbMrHfv-EXt2irAUo89yrru8hGSJw9R0iTyoytM"
                                     className="img-responsive" width="250" height="250"/></p>
                             <p>Samuel Youngs</p>
-                            <p>Quis vero earum ullam explicabo. Quae ratione rem illo!</p>
+                            <p>Computer science undergrad and front end developer.</p>
                           </div>
                           </div>
                           <div class="col-sm-6">
